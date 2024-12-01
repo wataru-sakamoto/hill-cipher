3695b7ae5b8dba9c3f089142807591da2f9d1dda
wataru-sakamoto <heisei123abc@gmail.com>
2024-12-01 16:21:24 +0900

    Add generate_key method to HillCipher class

--- hill-cipher/cipher.py
+++ hill-cipher/cipher.py
@@ -8,3 +8,10 @@ class HillCipher:
         """
         self.block_size = block_size
         self.key = None
+
+    def generate_key(self):
+        """
+        Generate an invertible matrix and set it as the key for this instance.
+        """
+        self.key = self._generate_invertible_matrix(self.block_size)
+        print("Generated encryption key (matrix):\n", self.key)
