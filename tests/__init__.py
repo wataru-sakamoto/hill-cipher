5933da1fed1b5e5063e16e9db6099b6e92643393
wataru-sakamoto <heisei123abc@gmail.com>
2024-12-10 21:19:25 +0900

    Add message __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is the __init__.py file for the tests directory.
+Use this file to define shared test utilities or configurations if needed.
+"""
+
+# Example: Import shared test utilities (if any)
+# from .test_utils import some_shared_function
