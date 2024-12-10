c006b1e67f422d2e50fee1266c63dcd3f3eacd0f
wataru-sakamoto <heisei123abc@gmail.com>
2024-12-10 21:36:50 +0900

    Add new test cases

---
+++ tests/test_cipher.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy as np
+from hillcipher.cipher import HillCipher
+
+class TestHillCipher(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up a HillCipher instance for testing.
+        """
+        self.cipher = HillCipher(block_size=2)
+        self.cipher.generate_key()
+
+    def test_key_generation(self):
+        """
+        Test that the generated key is a valid invertible matrix.
+        """
+        key = self.cipher.key
+        self.assertIsNotNone(key, "Key should not be None")
+        self.assertEqual(key.shape, (2, 2), "Key should be a 2x2 matrix")
+        self.assertTrue(HillCipher._is_invertible(key), "Key should be invertible")
